6777120a9d6e1193e15594cd55d9b187526c901b
joaobisi <jbisinotti@gmail.com>
2023-05-29 00:15:54 -0300

    criação da model sala que representa as salas de aula a serem criadas
    Co-authored-by: pedrohelias <pedrohelias95@hotmail.com>

--- lib/models/sala.dart
+++ lib/models/sala.dart
@@ -5,4 +5,13 @@ class Sala {
   List<Disciplina> disciplinas;
 
   Sala(this.nome) : disciplinas = [];
+
+  bool temAulaNoDia(int dia) {
+    for (var disciplina in disciplinas) {
+      if (disciplina.diasSemana.contains(dia)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
